53bcd5b95e0aa3a9500c96b6a17e336df053f468
James Chapman <James.Chapman02@gmail.com>
2015-10-12 12:07:27 +1300

    new files

---
+++ adtools.ps1
@@ -0,0 +1,42 @@
+﻿#get ad info
+
+Get-addomain | select Forest,Netbiosname,DomainMode,PDCEmulator,RIDMaster,Infratructuremaster
+
+Get-AdForest | select Name,Forestmode,Globalcatalogs,*Master
+
+# get all domain controllers
+
+$domain = get-addomain
+
+$DCs = get-addomaincontroller -filter { domain -eq #domain.dnsroot } 
+
+$DCs | select name,operatingsystem,ipv4address
+
+# verify services
+
+$services = "netlogon","NTDS","KDC","ADWS"."NTFRS"
+
+get-service $services -ComputerName $DCs.name | 
+sort machinename,name |
+select | name,displayname,status,machinename |
+Out-GridView -title "Ad Service Status"
+
+#check logs
+
+$lognames = 'Active Directory Web Services','Directory Service','File Replication Service','System'
+
+# get recent errors and warnings from these logs on all DC's
+
+$mylogs = foreach ($log in $lognames) {
+    Write-Host $log -ForegroundColor Yellow
+    #add eventlog name to output object
+    Get-EventLog $log  -ComputerName $DCs.name -Newest 10 -EntryType Error,Warning |
+    Add-Member -MemberType NoteProperty -Name Logname -Value $log -PassThru
+    }
+
+# view filter and analyse
+
+$mylogs | sort logname,machinename,timegenerated |
+select logname,machinename,timegenerated,eventid,entrytype,source,category,message |
+Out-GridView -Title "AD Log info"
+
---
+++ shares and perms.ps1
@@ -0,0 +1,69 @@
+﻿# creating fileshare using remote session
+
+$FS = "corpserv.corp.local"
+
+#create remoting session
+
+$remote = New-PSSession -ComputerName $FS
+
+$newpath = "C:\shares\newshare"
+
+Invoke-command {mkdir $using:newpath} -session $remote
+
+#using newpath ensure the remote session uses the local newpath variable (doesn't attempt to use $newpath on the remote machine)
+
+
+
+
+
+
+# modifying permissions on a share (specifically removing the everyone permission
+
+$path = 'C:\Users\James Chapman\Documents\Share'
+
+#get the acl
+
+$acl = get-acl -path $path
+
+#get specific access rights
+
+$access = $acl.Access 
+
+#specify the user you're interested in (create as ntaccount object)
+
+[System.Security.Principal.NTAccount] $principal="Builtin\Users"
+
+# grab the specific rule you want for 'builtin users'
+
+$rule = $access | where {$_.identityreference -eq $principal}
+
+#use all this to delete the rule
+
+$rule | foreach { $acl.RemoveAccessRuleSpecific($_) }
+
+# set the new ACL
+
+set-acl -path $path -AclObject $acl
+
+#check the outcome
+
+(get-acl $path).access 
+
+#you will see that everyone has gone
+
+
+
+
+# give Everyone Modify rights
+
+$acl = get-acl -path $path
+
+[System.Security.Principal.NTAccount] $principal="Builtin\Users"
+
+$right = "Modify"
+$rule = New-Object System.Security.AccessControl.FileSystemAccessRule($Principal,$Right,"Allow")
+$rule
+$acl.SetAccessRule($rule)
+Set-Acl -path $path -AclObject $acl
+
+
